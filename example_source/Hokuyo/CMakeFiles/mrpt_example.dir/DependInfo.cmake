0378d12692dd862836c13602fdc383324541c646
jsharp83 <jsharp83@gmail.com>
2009-12-02 17:15:41 +0900

    This source based on MRPT library
    Signed-off-by: jsharp83 <jsharp83@gmail.com>

---
+++ example_source/Hokuyo/CMakeFiles/mrpt_example.dir/DependInfo.cmake
@@ -0,0 +1,13 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jsharp/KAIR/example_source/Hokuyo/test.cpp" "/home/jsharp/KAIR/example_source/Hokuyo/CMakeFiles/mrpt_example.dir/test.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ example_source/Hokuyo/CMakeFiles/mrpt_example.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/mrpt_example.dir/test.o"
+  "mrpt_example.pdb"
+  "mrpt_example"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/mrpt_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
